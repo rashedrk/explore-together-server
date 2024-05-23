b907e43494158d6b05c024c89b8599266b3cf11e
rashedrk <rashedrk001@gmail.com>
2024-05-24 00:24:41 +0600

    bug fixes

--- src/app/middlewares/Auth.ts
+++ src/app/middlewares/Auth.ts
@@ -6,35 +6,35 @@ import { prisma } from '../utils/global';
 
 
 const auth = () => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.headers.authorization;
 
-    // checking if the token is missing
-    if (!token) {
-      throw new Error( 'You are not authorized!');
-    }
-
-    // checking if the given token is valid
-    const decoded = jwt.verify(
-      token,
-      config.jwt_secret as string,
-    ) as JwtPayload;
-
-    const { id } = decoded;
-   
-
-    // checking if the user is exist
-    const user = await prisma.user.findUnique({
-        where: {
-            id
+        // checking if the token is missing
+        if (!token) {
+            throw new Error('You are not authorized!');
         }
-    });
 
-    if (!user) {
-      throw new Error('This user is not found !');
-    }
-    next();
-  });
+        // checking if the given token is valid
+        const decoded = jwt.verify(
+            token,
+            config.jwt_secret as string,
+        ) as JwtPayload;
+
+        const { id } = decoded;
+
+
+        // checking if the user is exist
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        });
+
+        if (!user) {
+            throw new Error('This user is not found !');
+        }
+        next();
+    });
 };
 
 export default auth;
