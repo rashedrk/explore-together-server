1d189f62d9d084bb430aa0a1e0c6d6df9d2e201e
rashedrk <rashedrk001@gmail.com>
2024-05-23 17:01:12 +0600

    sendResponse util added and user email unique

--- src/app/modules/User/user.controllers.ts
+++ src/app/modules/User/user.controllers.ts
@@ -1,10 +1,14 @@
 import catchAsync from "../../utils/CatchAsync";
+import sendResponse from "../../utils/sendResponse";
 import { userServices } from "./user.services";
 
 const createUser = catchAsync(async (req, res) => {
         const result = await userServices.createUserIntoDB(req.body)
-        res.status(200).json({
-            result
+        sendResponse(res, {
+            statusCode: 200,
+            success: true,
+            message: 'User created successfully',
+            data: result
         })
     });
 
---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,22 @@
+import { Response } from "express"
+
+const sendResponse = <T>(res: Response, payload: {
+    statusCode: number,
+    success: boolean,
+    message: string,
+    meta?: {
+        page: number,
+        limit: number,
+        total: number
+    },
+    data: T | null | undefined
+}) => {
+    res.status(payload.statusCode).json({
+        success: payload.success,
+        message: payload.message,
+        meta: payload.meta || null || undefined,
+        data: payload.data || null || undefined
+    })
+}
+
+export default sendResponse;
